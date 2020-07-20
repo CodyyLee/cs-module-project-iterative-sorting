6a87bd67b56e8060a936fa79ad427d99594454b4
Cody Garrett <cody.l.garrett22@gmail.com>
2020-07-20 14:43:44 -0600

    complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in arr:
+        if i == target:
+            return arr.index(i)
 
     return -1   # not found
 
@@ -9,6 +11,23 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    found = False
+    start = 0
+    end = len(arr) - 1
+    last = 0
 
+    while not found:
+        middle = (start + end) // 2
+
+        if last == middle or last - middle == 1:
+            return -1
+        elif arr[middle] == target:
+            return arr.index(arr[middle])
+        elif target > arr[middle]:
+            start = middle
+            last = middle
+        else:
+            end = middle
+            last = middle
 
     return -1  # not found
